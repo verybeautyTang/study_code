e929d4eec361c5586f82a143aa9be3d9591ed7b6
wyx-pc <shawnwang@futunn.com>
2022-05-12 23:32:38 +0800

    feat: 合成事件池

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
+import React from 'react';
 import { GetState } from './state'
 import SetState from './setState'
 import OrderUpstate from './orderUpdate'
 import SuperContainer from './opderUpdateExtend'
+import NativeOrReact from './nativeOrReact';
 function App() {
   return (
     <div className="App">
+      <NativeOrReact/>
+      {/* <br/>
       <GetState />
       <br/>
       <SetState/>
       <br/>
       <OrderUpstate/>
       <br/>
-      <SuperContainer/>
+      <SuperContainer/> */}
     </div>
   );
 }
---
+++ react-study/src/nativeOrReact.js
@@ -0,0 +1,48 @@
+import React, { useRef, useEffect } from "react";
+const NativeOrReact = () => {
+  const refFather = useRef(null);
+  const refChild = useRef(null);
+  useEffect(() => {
+    refFather.current.addEventListener("click", () => {
+      console.log('我是绑定在原生上的父节点组件')
+    })
+    refChild.current.addEventListener("click", () => {
+      console.log('我是绑定在原生上的子节点组件')
+    })
+    document.addEventListener('click', () => {
+      console.log('我是绑定在document上的组件')
+    })
+    refFather.current.addEventListener("click", () => {
+      console.log('捕获我是绑定在原生上的父节点组件')
+    },true)
+    refChild.current.addEventListener("click", () => {
+      console.log('捕获我是绑定在原生上的子节点组件')
+    },true)
+    document.addEventListener('click', () => {
+      console.log('捕获我是绑定在document上的组件')
+    },true)
+  },[])
+  const fatherClick = () => {
+    console.log('我是绑定在React上面的父节点事件')
+  }
+  const childClick = () => {
+    console.log('我是绑定在React上面的子节点事件')
+  }
+  const fatherCapture = () => {
+    console.log('捕获我是挂载在React上的父节点事件')
+  }
+  const childCapture = () => {
+    console.log('捕获我是挂载在React上的子节点事件')
+  }
+  return (
+    <div>
+      <h1>Native or React?</h1>
+      <div  ref= {refFather} onClick={fatherClick} onClickCapture={fatherCapture}>
+        <div>💡 Look At Me I am Father</div>
+        <button ref= {refChild} onClick={childClick} onClickCapture={childCapture}>Yeal！ I am Child</button>
+      </div>
+    </div>
+  )
+}
+
+export default NativeOrReact;
