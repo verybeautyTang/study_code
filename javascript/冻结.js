32f5841e4a6b6bd929fd5459c320201bed72bde2
heby <heby@mingyuanyun.com>
2022-07-05 14:45:21 +0800

    fix: 深度冻结

--- javascript/冻结.js
+++ javascript/冻结.js
@@ -14,9 +14,21 @@ console.log(a.other)
 
 // freeze是浅冻结，需要深度遍历进入深冻结阶段
 
+// 深冻结函数。
+function deepFreeze(obj) {
 
-const deepFreeze = (obj) => {
-  if(typeof obj === 'object') {
-    
-  }
+  // 取回定义在 obj 上的属性名
+  var propNames = Object.getOwnPropertyNames(obj);
+
+  // 在冻结自身之前冻结属性
+  propNames.forEach(function(name) {
+    var prop = obj[name];
+
+    // 如果 prop 是个对象，冻结它
+    if (typeof prop == 'object' && prop !== null)
+      deepFreeze(prop);
+  });
+
+  // 冻结自身 (no-op if already frozen)
+  return Object.freeze(obj);
 }
