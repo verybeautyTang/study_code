baf36c322c99ba70081676bf552aa9e8d6e8be2c
verybeautyTang <53130945+verybeautyTang@users.noreply.github.com>
2022-07-28 13:06:52 +0800

    Create 二叉树前序遍历.js

---
+++ leetCode/二叉树前序遍历.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    let data  = []
+    const getConfig = (root)  => {
+        data.push(root.val);
+        if(root.left) getConfig(root.left)
+        if(root.right) getConfig(root.right)
+    }
+    if(root) getConfig(root)
+    return data
+};
