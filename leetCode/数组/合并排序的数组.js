57d9865adbfb4e81fb13caa35f805aa2752ab861
verybeautyTang <53130945+verybeautyTang@users.noreply.github.com>
2022-09-10 10:20:40 +0800

    Create 合并排序的数组.js

---
+++ leetCode/数组/合并排序的数组.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} A
+ * @param {number} m
+ * @param {number[]} B
+ * @param {number} n
+ * @return {void} Do not return anything, modify A in-place instead.
+ */
+var merge = function(A, m, B, n) {
+    A.splice(m, A.length - m, ...B);
+    A.sort((a, b) => a - b);
+};
