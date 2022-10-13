ea614a4744414f03c2ec6fbec16db2b064c08981
verybeautyTang <53130945+verybeautyTang@users.noreply.github.com>
2022-10-13 19:37:07 +0800

    Create 异步串形循环.js

---
+++ leetCode/异步串形循环.js
@@ -0,0 +1,38 @@
+// 串形异步循环
+const bucket = {
+  apple: 3,
+  pear: 2,
+  peach:1,
+  banana: 10,
+  kiwi: 8,
+  lemeon: 7,
+  mango: 5,
+}
+
+// 计数一种水果，每个水果需要话1s进行计时，最终输出水果的名称和水果的数量
+function countSingleFruits(fruit) {
+  return new Promise((resolve) => {
+    setTimeout(() =>  resolve(fruit),bucket[fruit] *1000);
+  }).then((res)=> {
+    console.log(`${bucket[fruit] }秒后`);
+    console.log(res)
+  })
+}
+
+// 第一种代码实现方式：[通过对象转化成数组，再用for of 的方式]
+function countAll1() {
+  let fruit = Object.keys(bucket)
+  for (let f of fruit) {
+    countSingleFruits(f);
+  }
+}
+
+// 第二种代码实现方式：[通过对象转化成数组，直接使用async await]
+async function countAll2() {  
+  let fruit = Object.keys(bucket);
+  for(let i  = 0; i < fruit.length; i++) {
+    await countSingleFruits(fruit[i]);
+  }
+}
+
+// countAll2()
