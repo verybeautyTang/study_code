7fb9e96136f8af7faa1c13aefb15d168a3f2ba64
verybeautyTang <53130945+verybeautyTang@users.noreply.github.com>
2022-10-21 22:18:06 +0800

    Create 二叉树的直径.js

---
+++ leetCode/leetcode热题100/二叉树的直径.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+    let res = 0;
+
+  /**
+   * @param {TreeNode} root
+   * @return {number} tree height
+   */
+  function dfs(node) {
+    if (!node) {
+      return 0;
+    }
+
+    const left = dfs(node.left);
+    const right = dfs(node.right);
+
+    res = Math.max(left + right, res);
+
+    return Math.max(left, right) + 1;
+  }
+
+  dfs(root);
+  return res;
+};
